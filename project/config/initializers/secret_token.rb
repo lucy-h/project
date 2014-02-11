e81d7d1cf08cd35be8d46608cc16972d01d9edf7
Lucy <peachie.monkey@gmail.com>
2014-02-10 23:27:24 -0500

    Added basic Rails framework with "Hello World" page added at /home

---
+++ project/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project::Application.config.secret_key_base = '242c6e78e282e0a28e5af1283d329f62acecd94ba6311e7f8a640e1dfa784f1b59cf98b0ba460a0a16ab11cda8345d078dc0613faf190c79b4e2f90139296274'
